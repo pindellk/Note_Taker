12f75862533c043cde0fbc462ba741a8675c8337
pindellk <pindellk@protonmail.com>
2020-11-05 21:17:14 -0500

    Clean up comments and syntax

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
+// Establish dependencies
 const path = require("path");
 const router = require("express").Router();
 
-
-// * GET `/notes` - Should return the `notes.html` file.
-router.get('/notes', (req, res) => {
+// GET `/notes` - returns the `notes.html` file
+router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-// * GET `*` - Should return the `index.html` file
-router.get('*', (req, res) => {
+// GET `*` - returns the `index.html` file
+router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
