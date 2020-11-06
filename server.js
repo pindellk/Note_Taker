6ef4ddeb583636716c8e1dc89e4ad9c2285c423d
pindellk <pindellk@protonmail.com>
2020-11-05 21:50:33 -0500

    Update html route syntax - /

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Establish dependencies
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,7 @@ app.use(express.json());
 
 // Set up routes
 app.use("/api/notes", require("./routes/apiRoutes"));
-app.use("/api/notes", require("./routes/htmlRoutes"));
+app.use("/", require("./routes/htmlRoutes"));
 
 // Server listener
 app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
