d12d1ca3ae04c75ae0df5bfe7acb6c64bdea58d0
pindellk <pindellk@protonmail.com>
2020-11-06 20:56:57 -0500

    Add route variables and update route paths

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Establish dependencies
 const express = require("express");
 const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set up routes
-app.use("/api/notes", require("./routes/apiRoutes"));
-app.use("/", require("./routes/htmlRoutes"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Server listener
 app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
