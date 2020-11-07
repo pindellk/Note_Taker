ab604cf5687e6f2ad58a1ba91010822b812a3b5f
pindellk <pindellk@protonmail.com>
2020-11-06 21:59:54 -0500

    Update read method

--- db/store.js
+++ db/store.js
@@ -10,30 +10,38 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFileAsync("db/db.json", "utf8");
+        // return readFileAsync("db/db.json", "utf8");
+        return readFileAsync("db/db.json", "utf8") 
+            .then((json) => {
+                return json;
+            })
+            .catch((err) => {
+                console.log("Error", err);
+            });
     }
 
-    write(note) {
-        return writeFileAsync("db/db.json", JSON.stringify(note));
-    }
+    // write(note) {
+    //     return writeFileAsync("db/db.json", JSON.stringify(note));
+    // }
 
-    getNotes() {
-        return this.read().then((notes) => {
-            let parseNotes;
+    // getNotes() {
+    //     return this.read().then((notes) => {
+    //         let parseNotes;
 
-            try {
-                parseNotes = [].concat(JSON.parse(notes));
-            } catch (err) {
-                parseNotes = [];
-            }
+    //         try {
+    //             parseNotes = [].concat(JSON.parse(notes));
+    //         } catch (err) {
+    //             parseNotes = [];
+    //         }
 
-            return parseNotes;
-        });
-    }
+    //         return parseNotes;
+    //     });
+    // }
     
-    addNote(note) {
-        return fs.appendFile("db/db.json", JSON.stringify(note));    
-    }
+    // needs testing
+    // addNote(note) {
+    //     return fs.appendFile("db/db.json", JSON.stringify(note));    
+    // }
 }
 
 module.exports = Store;
