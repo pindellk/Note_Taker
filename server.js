756975437ba70dbe95ce2be38907628224004b9e
pindellk <pindellk@protonmail.com>
2020-11-09 22:36:19 -0500

    Add __dirpath body parser middleware line

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 // Body parser middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// new 
+app.use(express.static(__dirname + '/public'));
 
 // Set up routes
 app.use("/api", apiRoutes);
