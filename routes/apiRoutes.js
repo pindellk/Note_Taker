7267e91860180d71e66e58e17910ead7c107922d
pindellk <pindellk@protonmail.com>
2020-11-09 22:55:05 -0500

    Update router.get noteStore method calls; remove if validation from router.post - no need for this as save button will not appear if no text is entered in text body, call noteStore.write

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,22 +8,19 @@ const noteStore = new Store();
 // GET `/api/notes` - reads the `db.json` file and returns all saved notes as JSON
 router.get('/notes', (req, res) => {        
     res.json(noteStore.read());
+    res.json(noteStore.getNotes());
 })
 
 // POST `/api/notes` - receives a new note to save on the request body, 
 // adds it to the `db.json` file, and then return the new note to the client.
 router.post("/notes", (req, res) => {
-    const newNote = {
+    const note = {
         id: uuid.v4(),
         title: req.body.title,
         text: req.body.text,
     };
-    if (!newNote.title) {
-        return res.status(400).json({ msg: "Be sure to add your note!"});
-    }
-    notes.push(newNote);
-    res.json(notes);
-})
+    noteStore.write(note);
+});
 
 // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete
 // In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with 
