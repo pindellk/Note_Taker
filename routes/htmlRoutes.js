a3c40e8c4ca1e36709fbcd33cfd7c1a241686766
pindellk <pindellk@protonmail.com>
2020-11-26 14:02:20 -0500

    Remove router dependency, use app for path requirements

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,15 @@
 // Establish dependencies
 const path = require("path");
-const router = require("express").Router();
 
-// GET `/notes` - returns the `notes.html` file
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+module.exports = (app) => {
+    // GET `/notes` - returns the `notes.html` file
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 
-// GET `*` - returns the `index.html` file
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+    // GET `*` - returns the `index.html` file
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
-module.exports = router;
-
+};
