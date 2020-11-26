37bf1393377efc34461f3ff810fd00562ad22452
pindellk <pindellk@protonmail.com>
2020-11-26 14:00:15 -0500

    Remove router dependency and usage, update routes, cleanup comments and formatting

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 // Establish dependencies
 const express = require("express");
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 5000;
 
-// Body parser middleware
+// Body parser middleware express app to handle data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// new 
 app.use(express.static(__dirname + '/public'));
 
-// Set up routes
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// Point our server to specific routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Server listener
-app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
+app.listen(PORT, () => {
+    console.log("App listening on PORT: " + PORT)
+});
 
